f93529835cd9c047866dddf09b9f35f54aadac11
Eli Vidal <vidaleli1989@gmail.com>
2018-06-05 10:14:12 +0800

    first commit
    family project

---
+++ script.js
@@ -0,0 +1,16 @@
+function welcome () {
+
+
+	var who= prompt("Is it your first time on our webiste?");
+	if (who=== "yes") {
+
+		alert("Welcome. We hope you will like it. It is built for you");
+	}
+	else if (who==="no") {
+
+		alert("Thank you for coming back. We appreciate your visit");
+	}
+}
+
+welcome();
+
